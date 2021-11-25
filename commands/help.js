25fbfeaac5d17d4bf338ab28e3663d84847cf1b1
Justin Tieu <rinkaandchan@outlook.com>
2021-11-25 17:14:58 +1100

    modular command system + nodemon (#9)
    * put events in separate files
    
    * moved to using nodemon to handle sessions
    
    * finished modular command system
    
    * extra shit

---
+++ commands/help.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+module.exports = {
+    name: "help",
+    match: matchType.SAME,
+    withPrefix: true,
+    description: "shows this menu",
+    execute(client, msg) {
+        // TODO: make the menu look nicer
+        
+        let ret = "Help\n\n";
+        
+        // read commands
+        fs.readdir('./commands/', (err, files) =>
+        {
+            files.forEach(file => {
+                const command = require(`../commands/${file}`)
+                if (command.withPrefix) {
+                    ret += "``-" + command.name + "``" + ` - ${command.description}\n`; // <- somebody please stop this 
+                }
+            })
+            
+            msg.channel.send(ret);
+        })
+    },
+};
---
+++ events/messageCreate.js
@@ -0,0 +1,62 @@
+const fs = require("fs");
+
+// matching enum type
+global.matchType = {
+    // always called, least restrictive
+    NONE: 0,
+    // must match the letters exactly, different casing is allowed.  wAng <-> wang
+    SAME: 1,
+    // must match the characters exactly, must have the same case. wang <-> wang and wAng </-> wang most restrictive
+    EXACT: 2,
+    // needs to contain the command as a space separated substring, different casing allowed wAng <-> big wang s
+    SUBSTRING: 3,
+    // needs to contain the command with the same casing wAng <-> big wAng s
+    SUBSTRING_EXACT: 4
+}
+
+module.exports = (client, msg) => {
+    const PREFIX = "-"
+    
+    if (msg.author === client.user)
+        return;
+
+    // read commands
+    fs.readdir('./commands/', (err, files) =>
+    {
+        files.forEach(file =>
+        {
+            const command = require(`../commands/${file}`)
+            
+            if (command.name === undefined) 
+                throw "command has no name tag"
+            
+            const usePrefix = command.withPrefix === undefined ? true : command.withPrefix;
+            const prefix = usePrefix ? PREFIX : "";
+            
+            switch (command.match) {
+                case matchType.NONE:
+                    command.execute(client, msg);
+                    break;
+                case matchType.SAME:
+                    // lowercase both ends to ensure shit never hits the fan
+                    if (msg.content.toLowerCase() === prefix + command.name.toLowerCase()) 
+                        command.execute(client, msg);
+                    break;
+                case matchType.EXACT:
+                    if (msg.content === prefix + command.name)
+                        command.execute(client, msg);
+                    break;
+                case matchType.SUBSTRING:
+                    if (msg.content.toLowerCase().includes(command.name.toLowerCase()))
+                        command.execute(client, msg);
+                    break;
+                case matchType.SUBSTRING_EXACT:
+                    if (msg.content.includes(command.name))
+                        command.execute(client, msg);
+                    break;
+                default:
+                    throw "shit hit the fan"
+            }
+        })
+    })
+}
